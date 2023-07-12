e6d305968e0f20352d5d6fb7989076bf3c3a50ef
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-07-13 01:59:46 +0530

    discord Frontend

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,52 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
-
-export default component$(() => {
+export default () => {
   return (
     <>
-      <Hero />
-      <Starter />
+       <div class="flex w-full min-h-screen  ">
 
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
+        <div class="bg-red-500 w-28 lg:flex flex-col items-center gap-4  hidden">
+          <div class="bg-gray-600 w-10 h-10 rounded-full mt-4"></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+          <div class="bg-gray-600 w-10 h-10 rounded-full "></div>
+         
         </div>
-      </div>
+
+        <div class="bg-yellow-500 w-32 flex flex-col justify-between lg:w-56">
+         <div class="bg-green-400 w-full h-40 flex flex-col gap-3 items-center justify-around ">
+          <div class= "bg-white lg:w-40  w-2/3 h-10"></div>
+          <div class= "bg-white lg:w-40  w-2/3 h-10"></div>
+         </div>
+
+         <div class="bg-slate-500 w-full  h-40 ">
+         <div class=" w-full h-40 flex flex-col  ">
+          <div class= "bg-cyan-500 grow flex flex-col"></div>
+            {/* <div class="bg-black w-full h-1/2" ></div>
+            <div class="bg-gray-600 w-full h-1/2"></div> */}
+          <div class= "bg-white w-full h-10"></div>
+          <div class= "bg-black w-full h-10"></div>
+         </div>
+         </div>
+        </div>
+
+        <div class="bg-purple-600 grow flex flex-col justify-between  ">
+          <div class="flex flex-col">
+          <div class="h-14 w-full bg-gray-600"></div>
+          </div>
+          <div class="flex flex-col">
+             <div class="h-14 w-full bg-gray-600 "></div>
+          </div>
+        </div>
+
+       </div>
     </>
   );
-});
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
 };
+
